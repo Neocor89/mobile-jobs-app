dc836d8386986e1590ed49b3b9f8b21886845579
Ben <ncor89b@gmail.com>
2023-03-23 00:06:10 +0100

    build: Create NearbyJobCard component

--- components/common/cards/nearby/NearbyJobCard.jsx
+++ components/common/cards/nearby/NearbyJobCard.jsx
@@ -1,14 +1,30 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity, Image } from 'react-native'
+import { checkImageUrl } from '../../../../ultis'
 
 import styles from './nearbyjobcard.style'
 
-const NearbyJobCard = () => {
+
+const NearbyJobCard = ({ job, handleNavigate }) => {
   return (
-    <View>
-      <Text>NearbyJobCard</Text>
-    </View>
+    <TouchableOpacity style={styles.container} onPress={handleNavigate}>
+      <TouchableOpacity style={styles.logoContainer}>
+          <Image
+            source={{ uri: checkImageUrl(job.employer_logo)
+              ? job.employer_logo
+              : 'https://res.cloudinary.com/dwoifuutn/image/upload/v1666282395/brand-logo-small_buu20k.png'
+            }}
+            resizeMode="contain"
+            style={styles.logoImage}
+          />
+      </TouchableOpacity>
+      <View style={styles.textContainer}>
+        <Text style={styles.jobName}  numberOfLines={1}>
+          {job.job_title}
+        </Text>
+        <Text style={styles.jobType}>{job.job_employement_type}</Text>
+      </View>
+    </TouchableOpacity>
   )
 }
 
-export default NearbyJobCard
+export default NearbyJobCard;
