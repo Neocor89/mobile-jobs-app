78ab68b9ce58724249fce1be9e6e56f7e5fdd003
Ben <ncor89b@gmail.com>
2023-04-05 22:40:40 +0200

    build: Setting env variable for API key

--- hook/useFetch.js
+++ hook/useFetch.js
@@ -1,19 +1,19 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
-// import { RAPID_API_KEY } from "@env";
+import { useState, useEffect } from "react";
+import { API_TOKEN } from "@env";
 
-// const rapidApiKey = RAPID_API_KEY;
+const rapidApiToken = API_TOKEN;
 
-const useFetch = (enpoint, query) => {
+const useFetch = (endpoint, query) => {
   const [data, setData] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
   const options = {
     method: "GET",
-    url: `https://jsearch.p.rapidapi.com/${enpoint}`,
+    url: `https://jsearch.p.rapidapi.com/${endpoint}`,
     headers: {
-      "X-RapidAPI-Key": "",
+      "X-RapidAPI-Key": `${rapidApiToken}`,
       "X-RapidAPI-Host": "jsearch.p.rapidapi.com",
     },
     params: { ...query },
